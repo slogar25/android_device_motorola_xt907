f5b91adedb1ee29011dfd17be3f9a9f7404d571c
slogar25 <michaelslogar@gmail.com>
2014-01-12 20:32:38 -0500

    bring in omni support

--- omni_xt907.mk
+++ omni_xt907.mk
@@ -1,8 +1,8 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
